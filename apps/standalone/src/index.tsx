831c4459db244c03e6c5cdb8917ccdae703801b4
Antranig Basman <antranig.basman@colorado.edu>
2023-03-31 12:34:21 +0100

    Configuration of standalone example for Howe Sound vascular plants scrollytelling integration

--- apps/standalone/src/index.tsx
+++ apps/standalone/src/index.tsx
@@ -8,9 +8,9 @@ root.render(
 	<TaxonPanel
 		taxonId={config.taxa[0].taxonId}
 		placeId={config.places[0].placeId}
-		dataSource={DataSource.url}
+		dataSource={DataSource.autoLoad}
 		config={config}
-		baseUrl="https://sisyphean.ca/inat"
+		baseUrl="."
 		itemWidth={180}
 		classes={{
 			tabs: "inat-tabs",
@@ -24,9 +24,9 @@ root.render(
 			tabDesc: "inat-tab-desc"
 		}}
 		tabDescs={{
-			recentDesc: "This page lists the most recent butterfly and moth observations made in BC. Click on any of them to go to the inaturalist site for more information.",
-			mostCommonDesc: "This page lists the most commonly reported species in BC. You can use the dropdown at the top right to filter the results to a particular year.",
-			mostFavouritedDesc: "Any time iNaturalist users encounter an observation they like, they can choose to \"favourite\" it. This page lists the most favourited moth and butterfly observations made in BC."
+			recentDesc: "This page lists the most recent observations of vascular plants made in the the Átl’ḵa7tsem/Howe Sound Biosphere Region. Click on any of them to go to the iNaturalist site for more information.",
+			mostCommonDesc: "This page lists the most commonly reported species in Átl’ḵa7tsem/Howe Sound. You can use the dropdown at the top right to filter the results to a particular year.",
+			mostFavouritedDesc: "Any time iNaturalist users encounter an observation they like, they can choose to \"favourite\" it. This page lists the most favourited vascular plants observations made in the the Átl’ḵa7tsem/Howe Sound Biosphere Region."
 		}}
 	/>
 );
